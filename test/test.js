cc54e4d74c420ff6f3cf8d165a3548975554f2cb
Dom Belcher <dominic.belcher@gmail.com>
2018-04-27 15:50:40 +0100

    added export of Request class by module index

--- test/test.js
+++ test/test.js
@@ -1,11 +1,18 @@
 const chai = require('chai')
 chai.should()
 
+// Test data
+const Request = require('../src/request')
+
 // Module under test
 const Utils = require('../src')
 
 describe('index tests', () => {
   it('should export an object', () => {
     Utils.should.be.an('object')
+  })
+
+  it('should export a Request object matching the one exported by loan.js', () => {
+    Utils.Request.should.deep.equal(Request)
   })
 })
