724c4d477630a0a411a1fcd19e66abd3d9876163
Dom Belcher <dominic.belcher@gmail.com>
2018-04-30 13:50:44 +0100

    removed redundant methods

--- src/request.js
+++ src/request.js
@@ -12,26 +12,6 @@ class Request extends Item {
     }
     super(data)
   }
-
-  // populate (requestData) {
-  //   this.data = _merge(this.data, _pick(requestData, fields))
-  //   return this
-  // }
-
-  // addExpiryDate (expiry_field = 'due_date') {
-  //   if (this.data[expiry_field]) {
-  //     this.data.expiry_date = moment(this.data[expiry_field]).unix()
-  //   }
-  //   return this
-  // }
-
-  // save () {
-  //   return this.db.save(this.data)
-  // }
-
-  // delete () {
-  //   return this.db.delete({ request_id: this.request_id })
-  // }
 }
 
 Request.fields = [
