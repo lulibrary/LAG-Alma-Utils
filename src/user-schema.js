d630cd4f18e4988431e4d29493d1e295857b95e6
Dom Belcher <dominic.belcher@gmail.com>
2018-05-22 16:07:47 +0100

    created user schema

---
+++ src/user-schema.js
@@ -0,0 +1,69 @@
+const dynamoose = require('dynamoose')
+const moment = require('moment')
+const _chunk = require('lodash.chunk')
+const _unionBy = require('lodash.unionby')
+
+const batchGetLimit = 25
+
+dynamoose.setDefaults({
+  create: false,
+  waitForActiveTimeout: 5000
+})
+
+const Schema = dynamoose.Schema
+
+const expiryOffset = {
+  value: 2,
+  unit: 'hours'
+}
+
+const calculateExpiry = () => {
+  return moment.add(expiryOffset.value, expiryOffset.unit).unix()
+}
+
+const userSchema = new Schema({
+  primary_id: {
+    type: String,
+    hashKey: true
+  },
+  loan_ids: [String],
+  request_ids: [String],
+  expiry_date: {
+    type: Number,
+    default: (model) => calculateExpiry(model.due_date)
+  }
+}, {})
+
+module.exports = (userTable) => {
+  if (userTable.region) {
+    dynamoose.AWS.config.update({
+      region: userTable.region
+    })
+  }
+
+  let model = dynamoose.model(userTable.name, userSchema)
+
+  model.prototype.addLoan = function (loanID) {
+    if (!this.loan_ids.includes(loanID)) {
+      this.loan_ids.push(loanID)
+    }
+  }
+
+  model.prototype.getLoanData = function (loanModel) {
+    this.loans = []
+
+    const loanIdBatches = _chunk(this.loan_ids, batchGetLimit)
+    let promises = []
+
+    loanIdBatches.forEach((batch) => {
+      const batchKeys = batch.map((loanID) => { return { loan_id: loanID } })
+      promises.push(loanModel.batchGet(batchKeys).then(loans => {
+        this.loans = this.loans.concat(loans)
+      }))
+    })
+
+    return Promise.all(promises).then(() => this.loans)
+  }
+
+  return model
+}
---
+++ test/user-schema-test.js
@@ -0,0 +1,152 @@
+const sinon = require('sinon')
+const sandbox = sinon.sandbox.create()
+
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const sinonChai = require('sinon-chai')
+chai.use(sinonChai)
+const should = chai.should()
+
+const rewire = require('rewire')
+
+const Model = require('dynamoose').Model
+const LoanSchema = require('../src/loan-schema')
+
+// Module under test
+const UserSchema = rewire('../src/user-schema')
+
+const testUserTable = {
+  name: 'usertable',
+  region: 'eu-west-2'
+}
+
+const TestUserModel = UserSchema(testUserTable)
+
+describe('user schema tests', () => {
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  describe('add method tests', () => {
+    it('should add a loan to the user if it does not already have the loan', () => {
+      let testUser = new TestUserModel({
+        primary_id: 'test user',
+        loan_ids: []
+      })
+
+      testUser.addLoan('a loan')
+
+      testUser.loan_ids.should.include('a loan')
+    })
+
+    it('should not add a loan if the user already has the loan', () => {
+      let testUser = new TestUserModel({
+        primary_id: 'test user',
+        loan_ids: ['a loan']
+      })
+
+      testUser.addLoan('a loan')
+
+      testUser.loan_ids.should.deep.equal(['a loan'])
+    })
+  })
+
+  describe('get loan data tests', () => {
+    it('should create an empty array of loans if no loan ids are present', () => {
+      let testUser = new TestUserModel({
+        primary_id: 'test user',
+        loan_ids: []
+      })
+
+      should.not.exist(testUser.loans)
+
+      return testUser.getLoanData(null).should.eventually.deep.equal([])
+    })
+
+    it('should call batchGet on the Loan model with an array of Keys', () => {
+      let testUser = new TestUserModel({
+        primary_id: 'test user',
+        loan_ids: ['1', '2', '3']
+      })
+
+      let testLoanModel = LoanSchema('table', 'region')
+      let batchGetStub = sandbox.stub(testLoanModel, 'batchGet')
+      batchGetStub.resolves([])
+
+      const expected = [{
+        loan_id: '1'
+      }, {
+        loan_id: '2'
+      }, {
+        loan_id: '3'
+      }]
+
+      return testUser.getLoanData(testLoanModel).then(() => {
+        batchGetStub.should.have.been.calledWith(expected)
+      })
+    })
+
+    it('should call batchGet on the Loan model twice if the number of loans is over the batchGet limit', () => {
+      let testUser = new TestUserModel({
+        primary_id: 'test user',
+        loan_ids: new Array(30).fill(0).map((value, index) => index.toString())
+      })
+
+      let testLoanModel = LoanSchema('table', 'region')
+      let batchGetStub = sandbox.stub(testLoanModel, 'batchGet')
+      batchGetStub.resolves([])
+
+      return testUser.getLoanData(testLoanModel).then(() => {
+        batchGetStub.should.have.been.calledTwice
+      })
+    })
+
+    it('should return a populated list of loans from the model#batchGet response', () => {
+      let testUser = new TestUserModel({
+        primary_id: 'test user',
+        loan_ids: ['1', '2', '3']
+      })
+
+      const expected = [{
+        loan_id: '1',
+        title: 'book1'
+      }, {
+        loan_id: '2',
+        title: 'book2'
+      }, {
+        loan_id: '3',
+        title: 'book3'
+      }]
+
+      let testLoanModel = LoanSchema('table', 'region')
+      let batchGetStub = sandbox.stub(testLoanModel, 'batchGet')
+      batchGetStub.resolves(expected)
+
+      return testUser.getLoanData(testLoanModel).then(() => {
+        testUser.loans.should.deep.equal(expected)
+      })
+    })
+
+    it('should fully populate the loan list if multiple batchGet requests are made', () => {
+      let testUser = new TestUserModel({
+        primary_id: 'test user',
+        loan_ids: new Array(30).fill(0).map((value, index) => index.toString())
+      })
+
+      const return1 = testUser.loan_ids.slice(0, 25).map(id => { return { loan_id: id, title: `book ${id}` } })
+      const return2 = testUser.loan_ids.slice(25).map(id => { return { loan_id: id, title: `book ${id}` } })
+
+      const expected = testUser.loan_ids.map(id => { return { loan_id: id, title: `book ${id}` } })
+
+      let testLoanModel = LoanSchema('table', 'region')
+      let batchGetStub = sandbox.stub(testLoanModel, 'batchGet')
+      batchGetStub.onCall(0).resolves(return1)
+      batchGetStub.onCall(1).resolves(return2)
+
+      return testUser.getLoanData(testLoanModel).then(() => {
+        testUser.loans.should.deep.equal(expected)
+      })
+    })
+  })
+})
