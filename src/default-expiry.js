73c65bf1f16091fc129c96c76a480a655e609032
Dom Belcher <dominic.belcher@gmail.com>
2018-08-07 11:19:49 +0100

    changed to using moment.js for setting 2 week expiry for clarity of time duration

--- src/default-expiry.js
+++ src/default-expiry.js
@@ -1,4 +1,6 @@
+const moment = require('moment')
+
 module.exports = {
   type: Number,
-  default: () => Math.floor(Date.now() / 1000) + 2 * 7 * 24 * 60 * 60
+  default: () => moment().add(2, 'weeks').unix()
 }
